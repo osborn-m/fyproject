5aebfbf509025340bb145fa727fe28b5c010e829
osborn-m <osbornm300@gmail.com>
2025-09-15 13:14:48 +0000

    Fix: add Render domain to ALLOWED_HOSTS

--- fyproject_backend/settings.py
+++ fyproject_backend/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-$!ztlvjac88-ja1v6&2v!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = ["fyproject-5zip.onrender.com", "localhost", "127.0.0.1"]
 
 # Application definition
 
