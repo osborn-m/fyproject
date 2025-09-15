2966c89ea7870547e71907f589aa71347d287664
osborn-m <osbornm300@gmail.com>
2025-09-15 21:46:39 +0000

    Added resources app with model, serializer, JSON import command, and initial data

--- fyproject_backend/settings.py
+++ fyproject_backend/settings.py
@@ -96,8 +96,6 @@ DATABASES = {
     },
 }
 
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
@@ -146,8 +144,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 1,
+   # "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+   # "PAGE_SIZE": 1,
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
